1225aa3411ef73cdd3c95e04bc4ec23c6d7b0ddc
Zenalghi <fatih.rafah17@gmail.com>
2025-09-17 19:52:45 +0700

    update seeder+comp

--- database/seeders/B_Merk.php
+++ database/seeders/B_Merk.php
@@ -18,11 +18,23 @@ class B_Merk extends Seeder
             ['id' => '0101', 'merk' => 'MITSUBISHI'],
             ['id' => '0102', 'merk' => 'HINO'],
             ['id' => '0103', 'merk' => 'ISUZU'],
+            ['id' => '0104', 'merk' => 'UDTRUCKS'],
+            ['id' => '0105', 'merk' => 'MERCEDES BENZ'],
+            ['id' => '0106', 'merk' => 'TATA'],
+            ['id' => '0107', 'merk' => 'FAW'],
+            ['id' => '0108', 'merk' => 'SUZUKI'],
+            ['id' => '0109', 'merk' => 'DAIHATSU'],
+
             // EURO 4 Merks
             ['id' => '0201', 'merk' => 'MITSUBISHI'],
             ['id' => '0202', 'merk' => 'HINO'],
             ['id' => '0203', 'merk' => 'ISUZU'],
-            // ... tambahkan data lain dari Excel Anda
+            ['id' => '0204', 'merk' => 'UDTRUCKS'],
+            ['id' => '0205', 'merk' => 'MERCEDES BENZ'],
+            ['id' => '0206', 'merk' => 'TATA'],
+            ['id' => '0207', 'merk' => 'FAW'],
+            ['id' => '0208', 'merk' => 'SUZUKI'],
+            ['id' => '0209', 'merk' => 'DAIHATSU'],
         ]);
     }
 }
