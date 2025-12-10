e31e147667c7159968d66c1d78b9a57ff81cf139
Zenalghi <fatih.rafah17@gmail.com>
2025-12-10 16:28:56 +0700

    all ok

--- app/Models/IGambarKelistrikan.php
+++ app/Models/IGambarKelistrikan.php
@@ -4,12 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class IGambarKelistrikan extends Model
 {
-    use HasFactory, SoftDeletes;
-
+    use HasFactory;
     protected $table = 'i_gambar_kelistrikan';
 
     protected $fillable = [
@@ -18,19 +16,14 @@ class IGambarKelistrikan extends Model
         'deskripsi',
     ];
 
-    // Relasi ke Master Data (untuk tahu ini milik varian apa)
     public function masterData()
     {
         return $this->belongsTo(MasterData::class, 'master_data_id');
     }
-
-    // Relasi ke File Fisik (untuk ambil PDF)
     public function fileKelistrikan()
     {
         return $this->belongsTo(MasterKelistrikanFile::class, 'master_kelistrikan_file_id');
     }
-
-    // Helper untuk mendapatkan path dengan mudah
     public function getPathGambarKelistrikanAttribute()
     {
         return $this->fileKelistrikan->path_file ?? null;
