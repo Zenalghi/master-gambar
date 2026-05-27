7d3e6e79fb2a7975e846902736b24640a012f517
Zenalghi <fatih.rafah17@gmail.com>
2026-05-28 03:05:58 +0700

    cnf

--- docker/mysql/healthcheck.sh
+++ docker/mysql/healthcheck.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 # Healthcheck script untuk MySQL
-# Membaca password dari environment variable yang di-set oleh docker-compose env_file
 mysqladmin ping -h localhost -u root -p"${MYSQL_ROOT_PASSWORD:-root_anti_ini}" > /dev/null 2>&1
